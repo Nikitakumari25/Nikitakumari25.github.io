77a85cc0760cab8fd8e27e2940bd32098defcbcf
Nikitakumari25 <137255568+Nikitakumari25@users.noreply.github.com>
2023-08-10 12:42:34 +0530

    Delete eccentric directory

--- eccentric/script.js
+++
@@ -1,8 +0,0 @@
-const header=document.querySelector("header .container");
-const hamburger=document.querySelector("header .hamburger");
-const navbar=document.querySelector("header #main-nav ul");
-
-hamburger.addEventListener("click",()=>{
-    hamburger.classList.toggle("active");
-    navbar.classList.toggle("active");
-});
